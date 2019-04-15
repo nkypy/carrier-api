9ff8819d0136160c6664aab373a5928a357a8266
Jack Shih <develop@kshih.com>
2019-04-15 21:39:40 +0800

    remove unused codes

--- src/client/mod.rs
+++
@@ -1,9 +0,0 @@
-mod carrier_client;
-mod china_telecom_client;
-mod china_unicom_client;
-mod china_mobile_client;
-mod guangdong_mobile_client;
-mod jiangsu_mobile_client;
-
-pub use crate::client::carrier_client::CarrierClient;
-pub use crate::client::guangdong_mobile_client::decrypt;
